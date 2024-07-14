e3820af110c6844ee0087000ad875ea104a07672
Ali Avada <110764863+3alifaisal@users.noreply.github.com>
2024-07-14 16:22:11 +0000

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+  moduleFileExtensions: ["js", "jsx"],
+};
